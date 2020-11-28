360baea0b0c4ccd50215eb591b8d170675afbd54
Tulasi <tulasireddykagithala@gmail.com>
2020-11-28 15:36:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/doctorapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.doctorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.signin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, OTPActivity.class));
+            }
+        });
+
+
+    }
+}
