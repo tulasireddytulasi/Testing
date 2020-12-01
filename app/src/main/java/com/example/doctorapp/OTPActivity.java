c0fe611207818b032515d51850c24aa339af1355
Tulasi <tulasireddykagithala@gmail.com>
2020-12-01 20:22:39 +0530

    New Home Page Added Third Commit

--- app/src/main/java/com/example/doctorapp/OTPActivity.java
+++ app/src/main/java/com/example/doctorapp/OTPActivity.java
@@ -1,14 +1,25 @@
 package com.example.doctorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class OTPActivity extends AppCompatActivity {
 
+    private CardView cardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_o_t_p);
+        cardView = findViewById(R.id.card);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/doctorapp/ui/home/HomeFragment.java
@@ -0,0 +1,54 @@
+package com.example.doctorapp.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.bumptech.glide.Glide;
+import com.example.doctorapp.R;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+    private ImageView imageView, img1, img2, img3;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        imageView = root.findViewById(R.id.profile_pic);
+        img1 = root.findViewById(R.id.img1);
+        img2 = root.findViewById(R.id.img2);
+        img3 = root.findViewById(R.id.img3);
+
+        Glide.with(getContext())
+                .load(R.drawable.profile_pic)
+                .into(imageView);
+
+        Glide.with(getContext())
+                .load(R.drawable.card)
+                .into(img1);
+
+        Glide.with(getContext())
+                .load(R.drawable.cards5)
+                .into(img2);
+
+        Glide.with(getContext())
+                .load(R.drawable.water)
+                .into(img3);
+
+
+
+        return root;
+    }
+}
